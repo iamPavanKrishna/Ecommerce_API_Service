2e10d22fa82d99d6763e0c52d18564aaee4a7723
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-10-07 18:17:46 +0530

    initial commit

---
+++ ecommerce_product_api_service/src/main/java/dev/pavan/ecommerce_product_api_service/EcommerceProductApiServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.pavan.ecommerce_product_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProductApiServiceApplication.class, args);
+	}
+
+}
---
+++ ecommerce_product_api_service/src/test/java/dev/pavan/ecommerce_product_api_service/EcommerceProductApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pavan.ecommerce_product_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
