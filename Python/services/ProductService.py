535318f07904bddd7352942ec4072de387157daa
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-10-11 01:16:45 +0530

    implemented services for Flask app

---
+++ Python/services/ProductService.py
@@ -0,0 +1,18 @@
+import zope.interface
+
+class ProductService(zope.interface.Interface):
+    zope.interface.Attribute('Attrizope')
+    def getallproducts():
+        pass
+    def getproductbyid(id):
+        pass
+    def createproduct(id, title, price, category, description, image):
+        pass
+    def updateproductbyid(id):
+        pass
+    def deleteproductbyid(id):
+        pass
+    def getallcategories():
+        pass
+    def getproductbycategory(category):
+        pass
---
+++ Python/services/ThirdPartyProductService.py
@@ -0,0 +1,82 @@
+import zope.interface
+from services import ProductService
+import json
+import requests
+
+@zope.interface.implementer(ProductService)
+class ThirdPartyProductService():
+    def getallproducts():
+        url = 'https://fakestoreapi.com/products'
+        resp = requests(url)
+
+        return resp.json()
+
+
+    def getproductbyid(id):
+        #TODO validations
+        url = f'https://fakestoreapi.com/products/{id}'
+        resp = requests(url)
+
+        return resp.json()
+    
+
+    def createproduct(id, title, price, category, description, image):
+        url = 'https://fakestoreapi.com/products'
+        data = {
+                    'title': title,
+                    'price': price,
+                    'description': description,
+                    'image': image,
+                    'category': category
+                }
+        data = json.loads(json.dumps(data))
+
+        resp = requests(url, json={
+            "method":"POST",
+            "body": data
+        })
+
+        return resp.json()
+    
+
+    def updateproductbyid(id, title, price, category, description, image):
+        url = 'https://fakestoreapi.com/products'
+        data = {
+                    'title': title,
+                    'price': price,
+                    'description': description,
+                    'image': image,
+                    'category': category
+                }
+        data = json.loads(json.dumps(data))
+
+        resp = requests(url, json={
+            "method":"PUT",
+            "body": data
+        })
+
+        return resp.json()
+    
+
+    def deleteproductbyid(id):
+        url = 'https://fakestoreapi.com/products'
+
+        resp = requests(url, json={
+            "method":"DELETE",
+        })
+
+        return resp.json()
+    
+
+    def getallcategories():
+        url = 'https://fakestoreapi.com/products/categories'
+        resp = requests(url)
+
+        return resp.json()
+    
+
+    def getproductbycategory(category):
+        url = f'https://fakestoreapi.com/products/category/{category}'
+        resp = requests(url)
+
+        return resp.json()
